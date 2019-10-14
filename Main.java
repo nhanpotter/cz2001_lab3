bc07f03f8656c261cadde30295d1ce5faf6acbbe
nhanpotter <nhanpotter8a@gmail.com>
2019-10-15 07:32:11 +0800

    Bug: Runtime in modified is greater than in original version

--- Main.java
+++ Main.java
@@ -1,9 +1,32 @@
-/*
-* The App generates random integers data set and runs sorting algorithm on it.
-* The sorting algorith is Mergesort and Modified Mergesort with Insertion sort
-* It count the number of key comparisons key comparisons and CPU times taken
-*/
 public class Main
 {
+    public static void main(String[] args) {
+        long[] result= new long[2];
+        long[] result2 = new long[2];
+        // Initialize
+        App app = new App(100, 3);
+        // Create an array with random integer
+        app.randomArrayGenerator();
+        result = app.modifiedMergeSort();
+        result2 = app.mergeSort();
+        printArray(result);
+        printArray(result2);
+    }
+
+    public static void printArray(long arr[]) {
+        int n = arr.length;
+        System.out.println("Array:");
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
     
 }
