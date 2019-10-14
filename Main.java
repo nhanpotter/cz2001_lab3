3becee35550b534ba99f4c2d15bbba803d249ab5
nhanpotter <nhanpotter8a@gmail.com>
2019-10-15 04:09:24 +0800

    [WIP] Main class

---
+++ Main.java
@@ -0,0 +1,9 @@
+/*
+* The App generates random integers data set and runs sorting algorithm on it.
+* The sorting algorith is Mergesort and Modified Mergesort with Insertion sort
+* It count the number of key comparisons key comparisons and CPU times taken
+*/
+public class Main
+{
+    
+}
