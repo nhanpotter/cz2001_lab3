dbeab9c1dc8e8fcbe5cb62fc40ec89f4d25020ea
slageveen <steven.lageveen@student.uva.nl>
2019-10-29 23:25:43 +0800

    Minor changes to what is stored in csv

--- lab4/src/App.java
+++ lab4/src/App.java
@@ -5,15 +5,17 @@ public class App
 {
     public static void main(String[] args) {
 		int[] sizeInputList = {10};
+		int[] randEdges = {0};
 		int[][] searchInputList = {{1,5}};
-		long[][] store = new long[sizeInputList.length][2];
+		long[][] store = new long[sizeInputList.length][3];
 		for (int i=0; i< sizeInputList.length; i++) {
 			Graph g = new Graph(sizeInputList[i]);
-//			g.addConnectedEdge(3);
-			g.generateRandomEdges(45);
+			g.addConnectedEdge(1);
+			g.generateRandomEdges(randEdges[i]);
 			g.printAdjacencyList();
 			store[i][0] = (long) sizeInputList[i];
 			store[i][1] = g.findShortestPath(searchInputList[i][0], searchInputList[i][1]);
+			store[i][2] = randEdges[i] + sizeInputList[i];
 		}
 
 		storeCSV("./lab4/data/file.csv", store);
