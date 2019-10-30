d2ce3958387bc6417d092eb7b30b9320b6ed2ecc
slageveen <steven.lageveen@student.uva.nl>
2019-10-30 16:47:13 +0800

    Changed how data is gathered and stored in the CSV file

--- lab4/src/App.java
+++ lab4/src/App.java
@@ -1,40 +1,52 @@
 import java.util.Scanner;
 import java.io.*;
 
-public class App
-{
+public class App {
     public static void main(String[] args) {
-		int[] sizeInputList = {10};
-		int[] randEdges = {0};
-		int[][] searchInputList = {{1,5}};
-		long[][] store = new long[sizeInputList.length][3];
-		for (int i=0; i< sizeInputList.length; i++) {
-			Graph g = new Graph(sizeInputList[i]);
-			g.addConnectedEdge(1);
-			g.generateRandomEdges(randEdges[i]);
-			g.printAdjacencyList();
-			store[i][0] = (long) sizeInputList[i];
-			store[i][1] = g.findShortestPath(searchInputList[i][0], searchInputList[i][1]);
-			store[i][2] = randEdges[i] + sizeInputList[i];
-		}
+        int[] sizes = new int[100];
+        sizes[0] = 100;
+        for (int i = 1; i < sizes.length; i++) {
+            sizes[i] = 100 * i;
+        }
+        int[] randEdges = {1000};
+        int[][] nodes = {{1, 5}};
+        long[][][] store = new long[sizes.length][randEdges.length][nodes.length];
 
-		storeCSV("./lab4/data/file.csv", store);
-	}
-	
-	private static void storeCSV(String filepath, long[][] store) {
+        for (int i = 0; i < sizes.length; i++) {
+            for (int j = 0; j < randEdges.length; j++) {
+                for (int k = 0; k < nodes.length; k++) {
+                    Graph g = new Graph(sizes[i]);
+                    g.addConnectedEdge(1);
+                    g.generateRandomEdges(randEdges[j]);
+                    g.printAdjacencyList();
+                    store[i][j][k] = g.findShortestPath(nodes[k][0], nodes[k][1]);
+                }
+            }
+        }
+
+        storeCSV("./lab4/data/file.csv", store, sizes, randEdges, nodes);
+    }
+
+    private static void storeCSV(String filepath, long[][][] store, int[] sizes, int[] numedges, int[][] nodes) {
         try {
             FileWriter writer = new FileWriter(filepath);
-
-            for (long[] store_elem : store) {
-                writer.append(String.join(", ", String.valueOf(store_elem[0]),
-                                            String.valueOf(store_elem[1])));
-                writer.append("\n");
+            writer.append("Sizes, Edges, Path: (A, B), CPU time\n");
+            for (int i = 0; i < store.length; i++) {
+                for (int j = 0; j < store[i].length; j++) {
+                    for (int k = 0; k < store[i][j].length; k++) {
+                        writer.append(Integer.toString(sizes[i]) + "," +
+                                Integer.toString(  (numedges[j] + sizes[i])) + "," +
+                                Integer.toString(nodes[k][0]) + "," + Integer.toString(nodes[k][1]) + "," +
+								Long.toString(store[i][j][k]));
+                        writer.append("\n");
+                    }
+                }
             }
 
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
     }
 }
